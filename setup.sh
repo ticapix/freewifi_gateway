ae87e49a7f70998eef8495ef999f1d0161fcbf49
ticapix <ticapix@gmail.com>
2016-10-09 23:20:43 +0000

    removing nodejs dep

--- setup.sh
+++ setup.sh
@@ -9,24 +9,6 @@ if [ $? -ne 0 ]; then
     cd -
 fi
 
-echo "checking nodejs"
-which node > /dev/null
-if [ $? -ne 0 ]; then
-    echo "nodejs not installed"
-    uname -m | grep --quiet armv6l
-    if [ $? -ne 0 ]; then
-	echo "trying default installer"
-	curl -sL https://deb.nodesource.com/setup_6.x | bash -
-	apt install -y nodejs
-    else
-	echo "armv6 detected. ugly install in /usr/local/"
-	curl -sL https://nodejs.org/dist/v6.7.0/node-v6.7.0-linux-armv6l.tar.xz | tar --directory /usr/local/ --strip-components=1 -xvJf -
-    fi
-fi
-
-echo "installing dependencies"
-#npm install phantomjs
-
 echo "checking for the python virtual environment"
 if [ ! -d venv3 ]; then
     echo "creating one"
@@ -39,3 +21,4 @@ echo "installing dependencies"
 pip install -r requirements.txt
 
 echo "installing daemon"
+echo "TODO"
